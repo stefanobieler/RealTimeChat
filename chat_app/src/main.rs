891132686b365de1f92b67cebd0ff9e01f87f1f4
Stefano Bieler <stefanobieler@gmail.com>
2022-03-24 17:22:23 -0700

    setup rocket

--- chat_app/src/main.rs
+++ chat_app/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::routes;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/world")]
+async fn world() -> &'static str {
+    return "Hello, World";
+}
+
+
+#[rocket::main]
+async fn main() {
+    return rocket::build()
+        .mount("/hello", routes![world])
+        .launch()
+        .await.unwrap();
 }
